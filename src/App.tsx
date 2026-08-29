779f360167c6f9732548a49229408934080b104d
Mohammadreza Kaviyani <kiyarash17kaviyani@gmail.com>
2026-08-29 21:37:16 +0330

    first commit and add Header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// components
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,70 @@
+import "./Header.css";
+
+// icons
+import { IoCartOutline } from "react-icons/io5";
+import { GoHome } from "react-icons/go";
+import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+export default function Header() {
+  const [mobileSearchBox, setMobileSearchBox] = useState(false);
+
+  
+
+  return (
+    <>
+      <div className="header_container">
+        <div className="content">
+          <div className="left">
+            <h2 className="logo">Mamzi's Mini Shop</h2>
+          </div>
+
+          <div className="center">
+            <div className="search_contaier">
+              <input type="text" placeholder="Search..." />
+            </div>
+          </div>
+
+          <div className="right">
+            <div
+              className="search_icon"
+              onClick={() => setMobileSearchBox(true)}
+            >
+              <span>Search</span>
+              <span>
+                <CiSearch />
+              </span>
+            </div>
+
+            <div className="cart">
+              <span>Cart</span>
+              <span className="badge">0</span>
+              <span>
+                <IoCartOutline />
+              </span>
+            </div>
+
+            <div className="home">
+              <span>Home</span>
+              <span>
+                <GoHome />
+              </span>
+            </div>
+          </div>
+
+          {mobileSearchBox && (
+            <>
+              <div className="mobile_search">
+                <input type="text" placeholder="search ..." />
+                <span  onClick={() => setMobileSearchBox(false)}>
+                  <IoClose />
+                </span>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
